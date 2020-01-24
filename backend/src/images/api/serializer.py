d3e77c4e38a5a81160e1d3c470458cdbcaaf9345
Jegor Sergeev <egor.f.sergeev@gmail.com>
2020-01-24 17:44:23 +0300

    Updated API to fit the frontend requirements

--- backend/src/images/api/serializer.py
+++ backend/src/images/api/serializer.py
@@ -2,11 +2,24 @@ from rest_framework import serializers
 from images.models import Image
 
 
+class ImageListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ['id',
+                  'title',
+                  'src',
+                  'width',
+                  'height']
+
+
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Image
-        fields = ['title',
-                  'file',
+        fields = ['id',
+                  'title',
+                  'src',
+                  'width',
+                  'height',
                   'author_name',
                   'source_date',
                   'source_link',
--- backend/src/images/models.py
+++ backend/src/images/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from versatileimagefield.fields import VersatileImageField
-from datetime import datetime
 
 
 class Image(models.Model):
     title = models.CharField(max_length=200)
 
-    file = VersatileImageField(
+    width = models.PositiveIntegerField('Image Width', null=True, blank=True)
+    height = models.PositiveIntegerField('Image Height', null=True, blank=True)
+
+    src = VersatileImageField(
         'Image',
         upload_to='images/',
+        width_field='width',
+        height_field='height'
     )
 
-    author_name = models.CharField(max_length=100)
-    source_date = models.DateField(default=datetime.now)
-    source_link = models.URLField()
+    author_name = models.CharField(max_length=100, null=True, blank=True)
+    source_date = models.DateField(null=True, blank=True)
+    source_link = models.URLField(null=True, blank=True)
 
     comment = models.TextField(null=True, blank=True)
 
