a4ee33b48038b476f6ab489661050869f76db02c
Jegor Sergeev <egor.f.sergeev@gmail.com>
2020-01-24 18:14:15 +0300

    Added POST requests to API

--- backend/src/images/api/urls.py
+++ backend/src/images/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ImageListView, ImageDetailedView
+from .views import ImageListView, ImageDetailedView, ImageCreateView
 
 urlpatterns = [
     path('images/', ImageListView.as_view()),
+    path('images/create/', ImageCreateView.as_view()),
     path('images/<pk>', ImageDetailedView.as_view())
 ]
--- backend/src/images/api/views.py
+++ backend/src/images/api/views.py
@@ -1,13 +1,18 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, ListCreateAPIView
 from images.models import Image
-from .serializer import ImageSerializer
+from .serializer import ImageSerializer, ImageListSerializer
 
 
 class ImageListView(ListAPIView):
     queryset = Image.objects.all()
-    serializer_class = ImageSerializer
+    serializer_class = ImageListSerializer
 
 
 class ImageDetailedView(RetrieveAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
+
+class ImageCreateView(ListCreateAPIView):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
